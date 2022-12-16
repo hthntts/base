8ab0fcf3c6d4a088c8582e4445b24e149f780d94
hthntts <hothienhaidl@gmail.com>
2022-12-16 12:20:55 +0700

    [cpanel] fix quota not found

--- setup.sh
+++ setup.sh
@@ -14,6 +14,7 @@ DAY=$(date +%Y-%m-%d)
 _DAY=$(date +%Y%m%d)
 TIME=$(date +%H-%M-%S)
 _TIME=$(date +%H%M%S)
+TEMPTEMP="${_DAY}${_TIME}"
 _ROOT_PATH=$(dirname "$0")
 TOOL_PATH=$_ROOT_PATH/tools
 TOOLS=$(ls $TOOL_PATH | grep -v '^_' | sort)
